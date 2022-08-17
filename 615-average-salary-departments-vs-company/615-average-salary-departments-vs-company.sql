ff4ab8fdaf67dfdc8e51a1a9110c1775d01099b4
Soumya Sreedhar <49044155+SoumyaSreedhar@users.noreply.github.com>
2022-08-17 00:19:22 -0700

    Update 615-average-salary-departments-vs-company.sql

--- 615-average-salary-departments-vs-company/615-average-salary-departments-vs-company.sql
+++ 615-average-salary-departments-vs-company/615-average-salary-departments-vs-company.sql
@@ -1,6 +1,14 @@
 /* Write your T-SQL query statement below */
+-- select CONCAT(YEAR(pay_date),'-',RIGHT(CONCAT('00', MONTH(pay_date)),2)) from Salary; -- gives the month in the format of 2022-08 
 
--- select CONCAT(YEAR(pay_date),'-',RIGHT(CONCAT('00', MONTH(pay_date)),2)) from Salary;
+-- Steps:
+-- 1. Write a cte to calculate avg for each department and each month.
+-- 2. Write a cte to calculate. avg for one whole department for entire compary for each 
+-- month without department id. That is avg over the mpt irrespective of departments.
+-- 3. Now the question ask for comparing each department's progrss vs the company for each month. so now since the month is common, join on pay_date.
+-- 4. Onece you get this make joins and write case statements.
+
+
 with cte as (select distinct S.pay_date, E.department_id,
 avg(S.amount) over(partition by E.department_id, S.pay_date) dep_avg
 from Salary S inner join Employee E
