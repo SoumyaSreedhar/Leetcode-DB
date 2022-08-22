01984688f5f96fea7c41edaa9488b77caadad72b
Soumya Sreedhar <49044155+SoumyaSreedhar@users.noreply.github.com>
2022-08-21 20:29:53 -0500

    Time: 857 ms (85.94%), Space: 0B (100.00%) - LeetHub

--- 1132-reported-posts-ii/1132-reported-posts-ii.sql
+++ 1132-reported-posts-ii/1132-reported-posts-ii.sql
@@ -2,11 +2,13 @@ with cte1 as
 (
 select 
     action_date,
-    (count(distinct r.post_id)/count(distinct a.post_id))*100 as percent
+    count(distinct r.post_id)*100/count(distinct a.post_id) perc
 from actions a
 left join removals r using (post_id) 
 where action = 'report' and extra = 'spam'
 group by 1
 )
 
-select round(avg(percent), 2) as average_daily_percent from cte1
+select round(avg(perc),2) average_daily_percent from cte1
+
+
