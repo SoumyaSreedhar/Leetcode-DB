ea4219c241fd98ee4c3200431f5fc1d05641d3f4
Soumya Sreedhar <49044155+SoumyaSreedhar@users.noreply.github.com>
2023-10-22 23:18:47 -0700

    Update 0342-power-of-four.py

--- 0342-power-of-four/0342-power-of-four.py
+++ 0342-power-of-four/0342-power-of-four.py
@@ -1,13 +1,7 @@
 '''
 Solution:
 logb to the base will always be awhole number if it is a perfect power of a number.
-4^2 = 16, applying log on both sides will give us log 16/log 4= 1 which is a whole number, similarly log 64/log4 =3 which is a whole nymber.
-
-
-
-
-
-
+4^2 = 16, applying log on both sides will give us log 16/log 4= 1 which is a whole number, similarly log 64/log4 =3 which is a whole number
 '''
 
 
