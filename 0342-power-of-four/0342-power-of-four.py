f7b7be53c21e5deb12683ec4d8c1521b965331ac
Soumya Sreedhar <49044155+SoumyaSreedhar@users.noreply.github.com>
2023-10-22 23:17:19 -0700

    Time: 32 ms (91.29%), Space: 16.2 MB (70.44%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.py
+++ 0342-power-of-four/0342-power-of-four.py
@@ -1,3 +1,16 @@
+'''
+Solution:
+logb to the base will always be awhole number if it is a perfect power of a number.
+4^2 = 16, applying log on both sides will give us log 16/log 4= 1 which is a whole number, similarly log 64/log4 =3 which is a whole nymber.
+
+
+
+
+
+
+'''
+
+
 class Solution:
     def isPowerOfFour(self, n: int) -> bool:
         if n<1:
