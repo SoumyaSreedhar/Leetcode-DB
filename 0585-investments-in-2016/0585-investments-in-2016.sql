9ad5e2d22394094470464f05f9b916289cc5a597
Soumya Sreedhar <49044155+SoumyaSreedhar@users.noreply.github.com>
2023-10-23 22:27:26 -0700

    Time: 962 ms (88.10%), Space: 0B (100.00%) - LeetHub

--- 0585-investments-in-2016/0585-investments-in-2016.sql
+++ 0585-investments-in-2016/0585-investments-in-2016.sql
@@ -1,15 +1,4 @@
 /* Write your T-SQL query statement below */
--- select distinct
--- -- a.tiv_2015 as tiv_15_a,
--- a.tiv_2016 as tiv_16_a,
--- -- b.tiv_2015 as tiv_15_b,
--- -- b.tiv_2016 as tiv_16_b,
--- concat(a.lat,a.lon) as city
--- from Insurance a
--- inner join Insurance b on
--- a.tiv_2015=b.tiv_2015 and a.pid<> b.pid
--- and concat(a.lat,a.lon) <> concat(b.lat,b.lon)
-
 with cte as (
 select tiv_2015,tiv_2016,concat(lat,lon) as city,
     count(*) over(partition by tiv_2015 ) as cnt,
